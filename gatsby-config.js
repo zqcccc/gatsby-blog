0a0b61d1197bbd29185d572590da22df9c3e593f
zqcccc <zhaoqiaochu@inke.cn>
2021-10-15 19:29:21 +0800

    fix inline code style

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 630,
+              maxWidth: 590,
             },
           },
           {
